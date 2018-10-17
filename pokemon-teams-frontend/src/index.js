a461b01a31234f32e1538c0a25fad68b9f35919f
Naeblis <johnmlowery@gmail.com>
2018-10-17 16:50:53 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,51 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    fetch("http://localhost:3000/trainers")
+      .then(response => response.json())
+      .then(response => {
+        const trainers = response;
+        renderTrainers(trainers);
+      });
+  });
+  
+  function renderTrainers(trainers) {
+    let main = document.getElementById("main");
+    trainers.forEach(trainer => {
+      main.innerHTML += ` 
+      <div class="card" id = "${trainer.id}" data-id="${trainer.id}"><p>"${
+        trainer.name
+      }"</p>
+      <button data-trainer-id="${trainer.id}">Add Pokemon</button>
+      <ul>
+      `;
+      trainer.pokemons.forEach(pokemon => {
+        let card = document.getElementById(pokemon.trainer_id);
+        card.lastElementChild.innerHTML += `
+        <li>${pokemon.nickname} 
+        (${pokemon.species}) 
+        <button class="release" data-pokemon-id="${
+          pokemon.id
+        }">Release</button></li>`;
+      });
+      main.innerHTML += `</ul>
+      </div>`;
+    });
+  }
+  
+  document.addEventListener("click", e => {
+    if (event.target.className === "release") {
+      // let trainerId = parseInt(
+      //   event.target.parentElement.parentElement.parentElement.dataset.id
+      // );
+      e.preventDefault();
+      let pokemonId = parseInt(event.target.dataset.pokemonId);
+      fetch(POKEMONS_URL + `/${pokemonId}`, {
+        method: "DELETE"
+      }).then(response => response.json());
+      event.target.parentElement.innerHTML = "";
+    }
+  });
