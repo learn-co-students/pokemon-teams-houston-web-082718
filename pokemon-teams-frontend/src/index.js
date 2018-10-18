33876d02489001ce211a30239a6eaf5c3e1f168a
kevinptx <kevin.peery@gmail.com>
2018-10-18 14:45:07 -0500

    Done.

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -28,8 +28,25 @@ function displayTrainers(data) {
     });
   });
 }
-
+//when adding pokemon to trainer, need to get the trainerObj
+// Then, add the pokemon to the trainerObj pokemon's array and push the added pokemon into the trainer Object's pokemon's array. Then when you check if it has 6 it's updating all the time.
+//
 function addPokemonToTrainer(pokemon, trainerId) {
+  // for (let i = 0; i < trainersData.length; i++) {
+  //   trainer = trainersData[i];
+  //   if (trainer.id === trainerId) {
+  //     let duplicate = false;
+  //     const pokeArr = trainer.pokemons;
+  //     for (let j = 0; j < pokeArr.length; j++) {
+  //       if (pokeArr[i].id === pokemon.id) {
+  //         duplicate = true;
+  //       }
+  //     }
+  //     if (duplicate === false) {
+  //       trainer.pokemons.push(pokemon);
+  //     }
+  //   }
+  // }
   trainerUl = document.querySelector(`[data-ul-id="${trainerId}"]`);
   trainerUl.innerHTML += `<li id=${pokemon.id}>${pokemon.nickname} (${
     pokemon.species
@@ -49,7 +66,10 @@ document.addEventListener("click", function(event) {
       return trainer.id === trainerId;
     });
     //figure out a way for this info to be updated. It's only seen when there's a snapshot of the page at refresh.
-    if (trainerObj.pokemons.length < 6) {
+    //look at the DOM instead of the database to see how many pokemons are there. you can find parent element.children.length and it will tell you how many are there. Determine what length that should be counting the corret children elements. There are other ways to do it looking at the database info but with what I have it's how I should move forward.
+    let pokemonLIs = event.target.parentElement.children[2].childElementCount;
+    debugger;
+    if (pokemonLIs < 6) {
       const data = {
         trainer_id: trainerId
       };
